683b72e6b4ba1a6bf5b929a58cf4edb5b0fba06d
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-20 16:13:31 +0300

    funtions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+   
+
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    sum = num1 + num2
+    return sum
 
 def halve(number):
-    pass
+    print(number/2)
+    dev = number/2
+    return dev
